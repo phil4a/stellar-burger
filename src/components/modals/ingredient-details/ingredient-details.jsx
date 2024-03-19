012ca5943e4e1a05ba4ca01df4032e0c72fc33c0
Dimitri <philjke@mail.ru>
2024-03-20 02:24:47 +0700

    добавить разметку модалки ингредиента

--- src/components/modals/ingredient-details/ingredient-details.jsx
+++ src/components/modals/ingredient-details/ingredient-details.jsx
@@ -1,9 +1,29 @@
 import styles from './ingredient-details.module.css';
-const IngredientDetails = ({ ...props }) => {
+const IngredientDetails = (props) => {
 	console.log(props);
 	return (
 		<div className={styles.body}>
 			<h2 className={`${styles.title} text text_type_main-large`}>Детали ингредиента</h2>
+			<img src={props.ingredient.image_large} className="mb-4" alt={props.ingredient.name} />
+			<h3 className="text text_type_main-medium mb-8">{props.ingredient.name}</h3>
+			<div className={`${styles.info} mb-5 text text_type_main-default text_color_inactive`}>
+				<div>
+					<p>Калории, ккал</p>
+					<p className="text text_type_digits-default">{props.ingredient.calories}</p>
+				</div>
+				<div>
+					<p>Белки, г</p>
+					<p className="text text_type_digits-default">{props.ingredient.proteins}</p>
+				</div>
+				<div>
+					<p>Жиры, г</p>
+					<p className="text text_type_digits-default">{props.ingredient.fat}</p>
+				</div>
+				<div>
+					<p>Углеводы, г</p>
+					<p className="text text_type_digits-default">{props.ingredient.carbohydrates}</p>
+				</div>
+			</div>
 		</div>
 	);
 };
