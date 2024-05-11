7baf3c8dae167ca12f53ca5a137ff8941a263564
Dimitri <philjke@mail.ru>
2024-05-11 22:54:14 +0700

    сделать Modal универсальным компонентом

--- src/components/modals/modal/modal.jsx
+++ src/components/modals/modal/modal.jsx
@@ -1,8 +1,6 @@
 import ReactDOM from 'react-dom';
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { setCurrentIngredient } from '../../../services/current-ingredient-slice';
+
 import ModalOverlay from '../modal-overlay/modal-overlay';
 import propTypes from 'prop-types';
 
@@ -10,13 +8,8 @@ import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './modal.module.css';
 
 const Modal = ({ children, onClose }) => {
-	const { ingredientId } = useParams();
-	const ingredients = useSelector((state) => state.ingredients.ingredients);
-	const dispatch = useDispatch();
-
 	const handleCloseModal = () => {
 		onClose();
-		dispatch(setCurrentIngredient({}));
 	};
 
 	useEffect(() => {
@@ -30,15 +23,6 @@ const Modal = ({ children, onClose }) => {
 			document.removeEventListener('keydown', handleEsc);
 		};
 	}, [onClose]);
-
-	useEffect(() => {
-		if (ingredientId) {
-			const ingredientData = ingredients.find((ing) => ing._id === ingredientId);
-			if (ingredientData) {
-				dispatch(setCurrentIngredient(ingredientData));
-			}
-		}
-	}, [ingredientId, dispatch, ingredients]);
 
 	const modalRoot = document.querySelector('#modals');
 
