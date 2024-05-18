f8d9667bdcd8e7598766b8162f07ad705f5db1b6
Dimitri <philjke@mail.ru>
2024-05-18 10:51:01 +0700

    типизация burger-ingredient

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,7 +1,7 @@
 import IngredientList from './ingredient-list/ingredient-list';
 import styles from './burger-ingredients.module.css';
 
-const BurgerIngredients = () => {
+const BurgerIngredients: React.FC = (): JSX.Element => {
 	return (
 		<section className={`${styles.section} pt-10 mr-10`}>
 			<h1 className="text text_type_main-large pb-5">Соберите бургер</h1>
