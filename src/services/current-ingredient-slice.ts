eb5c040259580063268a47f62ef970fe83eae831
phil <philjke@mail.ru>
2024-05-21 16:16:25 +0700

    типизация слайсов редакса

---
+++ src/services/current-ingredient-slice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { IIngredient } from '../utils/types';
+
+interface CurrentIngredientState {
+	ingredient: IIngredient | null;
+}
+
+const initialState: CurrentIngredientState = {
+	ingredient: null,
+};
+
+export const currentIngredientSlice = createSlice({
+	name: 'currentIngredient',
+	initialState,
+	reducers: {
+		setCurrentIngredient: (state, action: PayloadAction<IIngredient>) => {
+			state.ingredient = action.payload;
+		},
+	},
+});
+
+export const { setCurrentIngredient } = currentIngredientSlice.actions;
+
+export default currentIngredientSlice.reducer;
