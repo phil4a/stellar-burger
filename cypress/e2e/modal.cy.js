34f63bbf64253c7ba2fe3ac06ceaa6ecb774727d
Dimitri <philjke@mail.ru>
2024-08-05 11:41:59 +0700

    тест закрытия модального окна

--- cypress/e2e/modal.cy.js
+++ cypress/e2e/modal.cy.js
@@ -1,16 +1,34 @@
 describe('Modal', () => {
 	const ingredientToSearch = 'Ингредиент 1';
 
-	it('should open modal', function () {
+	beforeEach(() => {
 		cy.prepare();
 		cy.contains(ingredientToSearch).should('exist').click();
+	});
+
+	it('should open modal and check ingredient name', function () {
 		cy.contains('Детали ингредиента').should('exist');
 		cy.get('[data-test-id="ingredient-name"]').should('have.text', ingredientToSearch);
 	});
 
-	it('should find correct ingredient name', function () {
-		cy.prepare();
-		cy.contains(ingredientToSearch).should('exist').click();
-		cy.get('[data-test-id="ingredient-name"]').should('have.text', ingredientToSearch);
+	it('should close modal on press close icon', function () {
+		cy.get('[aria-label="Закрыть"]').should('exist').click();
+		cy.contains('Детали ингредиента').should('not.exist');
+	});
+
+	it('should close modal on press esc', function () {
+		cy.contains('Детали ингредиента').should('exist');
+		cy.get('body').type('{esc}');
+		cy.contains('Детали ингредиента').should('not.exist');
+	});
+
+	it('should close modal on click outside', function () {
+		cy.contains('Детали ингредиента').should('exist');
+		cy.get('[data-test-id="modal"]').then(($modal) => {
+			const modal = $modal[0];
+			const { top, right, bottom, left } = modal.getBoundingClientRect();
+			cy.get('body').click(left - 10, top - 10);
+		});
+		cy.contains('Детали ингредиента').should('not.exist');
 	});
 });
--- src/components/modals/modal-overlay/modal-overlay.tsx
+++ src/components/modals/modal-overlay/modal-overlay.tsx
@@ -1,13 +1,9 @@
 import styles from './modal-overlay.module.css';
-import propTypes from 'prop-types';
 
 const ModalOverlay: React.FC<{ onClose: React.MouseEventHandler<HTMLDivElement> }> = ({
 	onClose,
 }): React.ReactElement => {
 	return <div onClick={onClose} className={styles.backdrop}></div>;
-};
-ModalOverlay.propTypes = {
-	onClose: propTypes.func.isRequired,
 };
 
 export default ModalOverlay;
