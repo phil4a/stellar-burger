8324506ef5ce07ffb97ac30c178b1e850769ad39
Dimitri <philjke@mail.ru>
2024-08-06 21:46:40 +0700

    Вынес в константу текст заголовка модального окна ингредиента

--- cypress/e2e/modal.cy.js
+++ cypress/e2e/modal.cy.js
@@ -1,5 +1,6 @@
 describe('Modal', () => {
 	const ingredientToSearch = 'Ингредиент 1';
+	const modalTitleText = 'Детали ингредиента';
 
 	beforeEach(() => {
 		cy.prepare();
@@ -7,28 +8,28 @@ describe('Modal', () => {
 	});
 
 	it('should open modal and check ingredient name', function () {
-		cy.contains('Детали ингредиента').should('exist');
+		cy.contains(modalTitleText).should('exist');
 		cy.get('[data-test-id="ingredient-name"]').should('have.text', ingredientToSearch);
 	});
 
 	it('should close modal on press close icon', function () {
 		cy.get('[aria-label="Закрыть"]').should('exist').click();
-		cy.contains('Детали ингредиента').should('not.exist');
+		cy.contains(modalTitleText).should('not.exist');
 	});
 
 	it('should close modal on press esc', function () {
-		cy.contains('Детали ингредиента').should('exist');
+		cy.contains(modalTitleText).should('exist');
 		cy.get('body').type('{esc}');
-		cy.contains('Детали ингредиента').should('not.exist');
+		cy.contains(modalTitleText).should('not.exist');
 	});
 
 	it('should close modal on click outside', function () {
-		cy.contains('Детали ингредиента').should('exist');
+		cy.contains(modalTitleText).should('exist');
 		cy.get('[data-test-id="modal"]').then(($modal) => {
 			const modal = $modal[0];
 			const { top, right, bottom, left } = modal.getBoundingClientRect();
 			cy.get('body').click(left - 10, top - 10);
 		});
-		cy.contains('Детали ингредиента').should('not.exist');
+		cy.contains(modalTitleText).should('not.exist');
 	});
 });
